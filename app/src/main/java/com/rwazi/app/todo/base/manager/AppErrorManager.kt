f81859382e4890efa1447fcfef9c57a77ab20968
traderfinn0312-sudo <traderfinn0312@gmail.com>
2026-04-03 17:00:00 +0700

    refactor error handle

--- app/src/main/java/com/rwazi/app/todo/base/manager/AppErrorManager.kt
+++ app/src/main/java/com/rwazi/app/todo/base/manager/AppErrorManager.kt
@@ -1,7 +1,15 @@
 package com.rwazi.app.todo.base.manager
 
 import android.content.Context
+import androidx.credentials.exceptions.GetCredentialCancellationException
+import androidx.credentials.exceptions.GetCredentialException
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.google.firebase.auth.FirebaseAuthRecentLoginRequiredException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.rwazi.app.todo.R
 import com.rwazi.app.todo.base.extension.isConnected
 import com.rwazi.app.todo.base.type.ViewState
@@ -23,17 +31,15 @@ class AppErrorManager @Inject constructor(
     }
 
     override fun handleError(throwable: Throwable): ViewState.Error {
-        when (throwable) {
+        return when (throwable) {
             is UnknownHostException,
             is ConnectException,
             is SocketTimeoutException,
             is SSLHandshakeException,
             is CertificateException,
-                -> {
-                return if (context.isConnected()) {
-                    ViewState.Error(
-                        ERROR_CODE_TIMEOUT, context.getString(R.string.unknown_error)
-                    )
+            is FirebaseNetworkException -> {
+                if (context.isConnected()) {
+                    ViewState.Error(ERROR_CODE_TIMEOUT, context.getString(R.string.unknown_error))
                 } else {
                     ViewState.Error(
                         ERROR_CODE_OFFLINE,
@@ -43,14 +49,45 @@ class AppErrorManager @Inject constructor(
                 }
             }
 
+            is GetCredentialCancellationException -> {
+                // User cancelled the flow, usually we don't show an error toast
+                ViewState.Error(error = "", showError = false)
+            }
+
+            is GetCredentialException -> {
+                ViewState.Error(error = context.getString(R.string.error_google_sign_in_failed))
+            }
+
+            is FirebaseAuthException -> {
+                handleFirebaseAuthError(throwable)
+            }
+
             is ApiException -> {
-                return ViewState.Error(error = throwable.message ?: "")
+                ViewState.Error(error = throwable.message ?: context.getString(R.string.unknown_error))
             }
 
             else -> {
-                return ViewState.Error(error = context.getString(R.string.unknown_error))
+                ViewState.Error(error = context.getString(R.string.unknown_error))
             }
         }
+    }
+
+    private fun handleFirebaseAuthError(e: FirebaseAuthException): ViewState.Error {
+        val messageRes = when (e) {
+            is FirebaseAuthInvalidUserException -> R.string.error_user_not_found
+            is FirebaseAuthInvalidCredentialsException -> R.string.error_wrong_password
+            is FirebaseAuthUserCollisionException -> R.string.error_auth_failed
+            is FirebaseAuthRecentLoginRequiredException -> R.string.error_auth_failed
+            else -> {
+                when (e.errorCode) {
+                    "ERROR_WRONG_PASSWORD" -> R.string.error_wrong_password
+                    "ERROR_USER_NOT_FOUND" -> R.string.error_user_not_found
+                    "ERROR_TOO_MANY_REQUESTS" -> R.string.error_too_many_requests
+                    else -> R.string.error_auth_failed
+                }
+            }
+        }
+        return ViewState.Error(error = context.getString(messageRes))
     }
 
 }
