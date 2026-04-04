5c8caea4efefaf103082f216fe26cf76a856eefa
hoangvd <hoangvdsynapz@gmail.com>
2026-04-04 12:53:00 +0700

    fix bug log out

--- app/src/main/java/com/rwazi/app/todo/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/rwazi/app/todo/ui/settings/SettingsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.viewModelScope
 import com.rwazi.app.todo.base.BaseViewModel
 import com.rwazi.app.todo.data.local.DataStorageManager
 import com.rwazi.app.todo.domain.repository.AuthRepository
+import com.rwazi.app.todo.domain.repository.NoteRepository
 import com.rwazi.app.todo.util.DynamicBackgroundUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
@@ -15,7 +16,8 @@ import javax.inject.Inject
 class SettingsViewModel @Inject constructor(
     private val authRepository: AuthRepository,
     private val dataStorageManager: DataStorageManager,
-    private val themeUtils: DynamicBackgroundUtils
+    private val themeUtils: DynamicBackgroundUtils,
+    private val noteRepository: NoteRepository
 ) : BaseViewModel() {
 
     val isAutoTheme = dataStorageManager.isAutoTheme.stateIn(
@@ -41,6 +43,7 @@ class SettingsViewModel @Inject constructor(
     }
 
     suspend fun signOut() {
+        noteRepository.clearLocalData()
         authRepository.signOut()
     }
 }
